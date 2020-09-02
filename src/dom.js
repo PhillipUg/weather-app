f3d26443f0470d1ec953b9394918320685a0c512
Phillip Musiime <phillipmusiime@gmail.com>
2020-09-02 17:08:43 +0300

    separated dom accessing variables

---
+++ src/dom.js
@@ -0,0 +1,11 @@
+const temperature = document.getElementById('temperature');
+const summary = document.getElementById('summary');
+const city = document.getElementById('city');
+const windy = document.getElementById('wind');
+const cloudy = document.getElementById('cloud');
+const humid = document.getElementById('humidity');
+const pres = document.getElementById('pressure');
+const bottom = document.querySelector('.bottom');
+const currentDate = document.getElementById('date');
+
+export { temperature, summary, city, windy, cloudy, humid, pres, bottom, currentDate };
