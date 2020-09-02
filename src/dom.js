24141fdae08ecbb87be6505f4034731004c7c040
Phillip Musiime <phillipmusiime@gmail.com>
2020-09-02 20:02:28 +0300

    accessing search btn, input field and error message and exporting them

--- src/dom.js
+++ src/dom.js
@@ -6,6 +6,10 @@ const cloudy = document.getElementById('cloud');
 const humid = document.getElementById('humidity');
 const pres = document.getElementById('pressure');
 const bottom = document.querySelector('.bottom');
+const search = document.querySelector('.btn');
 const currentDate = document.getElementById('date');
+const input = document.getElementById('search');
+const error = document.querySelector('.error');
 
-export { temperature, summary, city, windy, cloudy, humid, pres, bottom, currentDate };
+
+export { temperature, summary, city, windy, cloudy, humid, pres, bottom, currentDate, search, input, error };
